4bd4663f2a71ba066faed072da870109017fdaf0
Kevin Tran <ktran@tack.tech>
2017-12-26 01:04:45 -0800

    add more dependencies and first screens

--- client/App.js
+++ client/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+import WelcomeScreen from './WelcomeScreen.js';
+import HomeScreen from './HomeScreen.js';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+const NavApp = StackNavigator({
+    Welcome: { screen: WelcomeScreen },
+    Home: { screen: HomeScreen }
 });
+
+export default NavApp;
